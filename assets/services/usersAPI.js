8ddac7625b8ab03744e34cd400d8e5093df2a7f4
abdousadik <sadik.abdelfettah@gmail.com>
2021-02-27 11:56:30 +0100

    config

--- assets/services/usersAPI.js
+++ assets/services/usersAPI.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+import { USERS_API } from '../config';
 
 function findAll() {
     return axios
-        .get("http://localhost:8000/api/users")
+        .get(USERS_API)
         .then(response => response.data['hydra:member'])
 }
 
 function register(user) {
-    return axios.post("http://localhost:8000/api/users", user);
+    return axios.post(USERS_API, user);
 }
 
 export default {
